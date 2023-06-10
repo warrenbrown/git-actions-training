983227e09f5f1607a2a98b117fa48294758dd37a
Warren Brown <warren@katapultmedia.com>
2023-06-10 11:28:31 -0500

    chore: ran prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,10 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: 'airbnb-base',
+  extends: "airbnb-base",
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  rules: {
-  },
+  rules: {},
 };
